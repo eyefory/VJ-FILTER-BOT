93bab12a99b1d52d1321998f15951685ebb7ea98
eyefory <k.charantej7793@gmail.com>
2025-01-18 16:51:35 +0530

    info.py

--- info.py
+++ info.py
@@ -11,7 +11,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
+API_ID = int(environ.get('API_ID', '22719348'))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
