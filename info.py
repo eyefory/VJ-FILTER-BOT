6f57d1c9d924c4051809d22983ca9dae01cba403
eyefory <k.charantej7793@gmail.com>
2025-01-18 17:16:39 +0530

    info.py

--- info.py
+++ info.py
@@ -59,9 +59,9 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kcharantej7793:ebkzPxhvPHPczUR7@cluster0.zrsgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fladofficial777:n1d643xtGzpy5Lq8@cluster0.6x1lp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'fladofficial777')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
